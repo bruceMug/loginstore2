c881bf1365d6c24d2cd13185c4a03cd9bc5a8282
brucemug <mugzbru199@gmail.com>
2024-01-24 08:32:45 +0300

    set up the signupscreen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import Animated, {
+  FadeIn,
+  FadeInDown,
+  FadeInUp,
+  FadeOut,
+} from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
 
 const SignupScreen = () => {
-    return (
-        <View>
-            <Text>Signup Screen</Text>
+  const navigation = useNavigation();
+  return (
+    <View className="bg-white h-full w-full">
+      <StatusBar style="light" />
+      <Image
+        className="h-full w-full absolute"
+        source={require("../assets/images/background.png")}
+      />
+
+      {/* lights */}
+      <View className="flex-row justify-around w-full absolute">
+        <Animated.Image
+          entering={FadeInUp.delay(200).duration(1000).springify()}
+          className="h-[190] w-[75]"
+          source={require("../assets/images/light.png")}
+        />
+        <Animated.Image
+          entering={FadeInUp.delay(400).duration(1000).springify()}
+          className="h-[140] w-[55]"
+          source={require("../assets/images/light.png")}
+        />
+      </View>
+
+      {/* form */}
+      <View className="h-full w-full flex justify-around pt-45">
+        {/* Title */}
+        <View className="flex items-center">
+          <Animated.Text
+            entering={FadeInUp.duration(1000).springify()}
+            className="text-white font-bold tracking-wider text-4xl"
+          >
+            Sign Up
+          </Animated.Text>
         </View>
-    );
+
+        {/* form */}
+        <View className="flex items-center mx-5 space-y-4 pt-10">
+          <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-3 rounded-2xl w-full">
+            <TextInput placeholder="Username" placeholderTextColor={"gray"} />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-3 rounded-2xl w-full">
+            <TextInput placeholder="Email" placeholderTextColor={"gray"} />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="bg-black/5 p-3 rounded-2xl w-full">
+            <TextInput placeholder="Password" placeholderTextColor={"gray"} secureTextEntry/>
+          </Animated.View>
+
+          <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className="w-full" >
+            <TouchableOpacity className="bg-sky-400 p-3 rounded-2xl w-full mb-3">
+              <Text className="text-l font-bold text-white text-center">
+                SignUp
+              </Text>
+            </TouchableOpacity>
+          </Animated.View>
+
+          <Animated.View
+            entering={FadeInDown.delay(800).duration(1000).springify()}
+            className="flex-row justify-center"
+          >
+            <Text>Already have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.push("Login")}>
+              <Text className="text-sky-600 font-bold"> Login</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default SignupScreen;
