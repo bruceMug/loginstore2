99e184d2818ca3dee7e5962089800d7acc96f681
brucemug <mugzbru199@gmail.com>
2024-02-01 10:32:50 +0300

    added stack navigation

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import OnBoardingScreen from '../screens/onBoardingScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function AuthStack(){
     return (
-        <View>
-            <Text>AuthStacks</Text>
-        </View>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name='Onboarding' component={OnBoardingScreen} />
+            <Stack.Screen name='Login' component={LoginScreen} />
+        </Stack.Navigator>
     );
 };
 
