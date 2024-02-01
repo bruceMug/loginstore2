28c3988fff5ba3a06d5ede2845b3427079dda363
brucemug <mugzbru199@gmail.com>
2024-02-01 10:32:36 +0300

    added nav container

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import AuthStack from './navigation/AuthStack';
 
 export default function App() {
   return (
-    <AuthStack />
+    <NavigationContainer>
+      <AuthStack />
+      {/* normally I add the <AppStack /> after for the other app logic */}
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
