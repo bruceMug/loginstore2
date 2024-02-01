07eb4cda1e02403a27c6e23285f6170e3290fdc3
brucemug <mugzbru199@gmail.com>
2024-02-01 11:05:23 +0300

    added safe area view

--- screens/onBoardingScreen.js
+++ screens/onBoardingScreen.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 
-const OnBoardingScreen = () => {
-    return (
-        <View>
-            <Text>Welcome to the Onboarding Screen!</Text>
-        </View>
-    );
+const OnBoardingScreen = ({ navigation }) => {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+      }}
+    >
+      <View>
+        <Text style={{fontSize: 30, fontWeight: "bold", color: "#20315f" }}>
+          GAME ON
+        </Text>
+      </View>
+
+      <View>
+        <Text>Image</Text>
+      </View>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <Text>Let's Begin</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 };
 
 export default OnBoardingScreen;
