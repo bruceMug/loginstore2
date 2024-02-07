67fccf8d7b496fa9e17e81fda37bec8375fb0aa0
brucemug <mugzbru199@gmail.com>
2024-02-07 06:27:20 +0300

    added safearea view

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, Image } from 'react-native';
+
+
 
 const LoginScreen = () => {
     return (
-        <View>
+        <SafeAreaView style={{flex: 1,}}>
+            <Image source={require('../assets/images/gaming.jpg')} style={{height: 200, width: 300, borderRadius: 20}}/>
             <Text>Login Screen</Text>
-        </View>
+        </SafeAreaView>
     );
 };
 
